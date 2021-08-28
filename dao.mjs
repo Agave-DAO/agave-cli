c8d40b0b221d6b96715d22b269411280bd1a0e3c
honeyismoney <honeyismoney@protonmail.com>
2021-08-28 15:23:03 +0400

    fix exports

--- dao.mjs
+++ dao.mjs
@@ -1,13 +1,11 @@
 // testing garden https://aragon.1hive.org/#/0xa0aaf413a765a8a4423f097167a2669c8e066bc3
-export default {
-  dao: '0xa0aAf413a765A8A4423f097167a2669C8E066Bc3',
-  acl: '0x595214c840fe12038c0d01b5506b31f7b9f08b9b',
-  token_manager: '0x8aebc77a2244281ed5a79d0416688ea32c1e0e2b',
-  cv_agent: '0xf36b0b6f145f1982c2feeeaa7239e6d332002c47',
-  tao_agent: '0x0a38e1523c271a7d59864383dbc4a02ecbdc1911',
-  emergency_admin_agent: '', // <-- this should be a separate DAO that any of the mutisig holders can call. no voting required
-  tao_voting: '0x56946de10d67b6a9e85df5d6e5dd1868822c5503',
-  cv_voting: '0xcb0402b2ee22f7dcc1320cb5e492a017d33a8b93',
-  aggregator: '0x852c396de592ce0febda67b007d64d2ecb964485',
-  agreement: '0xf3fa25bdd692d7145a2c02dff6212349502b9167',
-}
+export const dao = '0xa0aAf413a765A8A4423f097167a2669C8E066Bc3'
+export const acl = '0x595214c840fe12038c0d01b5506b31f7b9f08b9b'
+export const token_manager = '0x8aebc77a2244281ed5a79d0416688ea32c1e0e2b'
+export const cv_agent = '0xf36b0b6f145f1982c2feeeaa7239e6d332002c47'
+export const tao_agent = '0x0a38e1523c271a7d59864383dbc4a02ecbdc1911'
+export const emergency_admin_agent = '' // <-- this should be a separate DAO that any of the mutisig holders can call. no voting required
+export const tao_voting = '0x56946de10d67b6a9e85df5d6e5dd1868822c5503'
+export const cv_voting = '0xcb0402b2ee22f7dcc1320cb5e492a017d33a8b93'
+export const aggregator = '0x852c396de592ce0febda67b007d64d2ecb964485'
+export const agreement = '0xf3fa25bdd692d7145a2c02dff6212349502b9167'
