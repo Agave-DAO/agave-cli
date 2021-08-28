a37fd164ecfa573ee0b81368ffa8f5f7df37b63a
greenhornet <HoneyIsMoney@protonMail.com>
2021-08-29 01:51:52 +0400

    rename to test

--- src/main.js
+++ src/main.js
@@ -1,20 +1,45 @@
-import Agave from './Agave.mjs'
+import prompts from './cli/prompts/index.mjs'
+import inquirer from 'inquirer'
 
-const run = async () => {
-  // create the vote
-  //const agveToken = '0x3a97704a1b25F08aa230ae53B352e2e72ef52843'
-  //await Agave.garden.addToken(agveToken)
-
-  // wait for the vote to pass then
-  const voteId = 0
-  const voteCallData =
-    '0x00000001853ad15ada188c43911d7c63c14b77c3d26dafa1000000640f3d5d970000000000000000000000003a97704a1b25f08aa230ae53b352e2e72ef5284300000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001'
-  await Agave.garden.executeVote(voteCallData, voteId)
+const mainMenu = async () => {
+    const result = await inquirer.prompt(prompts.menu.main)
+    return result.main
 }
 
-run()
-  .then(() => process.exit(0))
-  .catch((e) => {
+const gardenMenu = async () => {
+    const result = await inquirer.prompt(prompts.menu.garden)
+    return result.garden
+}
+
+const lendingPoolMenu = async () => {
+    const result = await inquirer.prompt(prompts.menu.lendingPool)
+    return result.lendingPool
+}
+
+const emergencyAgentMenu = async () => {
+    const result = await inquirer.prompt(prompts.menu.emergencyAgent)
+    return result.emergencyAgent
+}
+
+const main = async () => {
+    const option = await mainMenu()
+    console.log(option)
+    switch(option) {
+        case 'garden':
+            await gardenMenu()
+            break
+        case 'lendingPool':
+            await lendingPoolMenu()
+            break
+        case 'emergencyAgent':
+            await emergencyAgentMenu()
+            break
+    } 
+    console.log('done!')
+}
+main()
+.then(() => process.exit(0))
+.catch((e) => {
     console.error(e)
     process.exit(1)
-  })
+})
