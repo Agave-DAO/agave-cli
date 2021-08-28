56984a789d05ec546783d802aa2dda118e093b74
honeyismoney <honeyismoney@protonmail.com>
2021-08-28 15:46:28 +0400

    update index

--- src/garden/index.mjs
+++ src/garden/index.mjs
@@ -1,5 +1,6 @@
+import addTokenAndPermissionaddToken from './addTokenAndPermission.mjs'
 import addToken from './addToken.mjs'
 import executeVote from './executeVote.mjs'
 import faucet from './faucet.mjs'
 
-export { addToken, executeVote, faucet }
+export { addTokenAndPermissionaddToken, addToken, executeVote, faucet }
