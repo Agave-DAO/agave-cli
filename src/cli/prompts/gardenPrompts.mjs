637d657560e466cd823c8311d07a152d614976a4
honeyismoney <honeyismoney@protonmail.com>
2021-08-29 15:28:42 +0400

    add address validation

--- src/cli/prompts/gardenPrompts.mjs
+++ src/cli/prompts/gardenPrompts.mjs
@@ -1,30 +1,32 @@
+import validate from '../handlers/validate.mjs'
 const addToken = [
-    {
-        type: 'input',
-        name: 'token',
-        message: 'Enter token Address: ',
-    },
-    {
-        type: 'confirm',
-        name: 'createPermission',
-        message: 'do you also want to create the ADD_POWER_SOURCE_ROLE?'
-    }
+  {
+    type: 'input',
+    name: 'token',
+    message: 'Enter token Address: ',
+    validate: (answer) => validate.address(answer),
+  },
+  {
+    type: 'confirm',
+    name: 'createPermission',
+    message: 'do you also want to create the ADD_POWER_SOURCE_ROLE?',
+  },
 ]
 
 const executeVote = [
-    { 
-        type: 'number',
-        name: 'voteId',
-        message: 'Enter voteID...'
-    },
-    { 
-        type: 'input', 
-        name: 'calldata',
-        message: 'Input the vote calldata...'
-    }
+  {
+    type: 'number',
+    name: 'voteId',
+    message: 'Enter voteID...',
+  },
+  {
+    type: 'input',
+    name: 'calldata',
+    message: 'Input the vote calldata...',
+  },
 ]
 
 export default {
-    addToken,
-    executeVote,
+  addToken,
+  executeVote,
 }
