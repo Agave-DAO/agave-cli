20fc5b65438fb2c1b970cf33e0fac43bca2e1c91
greenhornet <HoneyIsMoney@protonMail.com>
2021-08-29 11:57:18 +0400

    add lending pool prompts

--- src/main.js
+++ src/main.js
@@ -25,10 +25,12 @@ const gardenMenu = async () => {
             selection = await inquirer.prompt(prompts.garden.addToken)
             // TODO:
             console.log('handeling..', selection)
+            break
         case 'executeVote':
             selection = await inquirer.prompt(prompts.garden.executeVote)
             // TODO:
-            console.log('handeling..', selection)    
+            console.log('handeling..', selection)   
+            break 
     }
 }
 
@@ -38,16 +40,97 @@ const emergencyAgentMenu = async () => {
     switch(result.emergencyAgent){
         case 'pauseProtocol':
             selection = await inquirer.prompt(prompts.emergencyAgent.pauseProtocol)
-            console.log('handeling..', selection)
+            // TODO:
+            console.log('handeling..', selection)   
+            break 
         case 'unpauseProtocol':
             selection = await inquirer.prompt(prompts.emergencyAgent.unpauseProtocol)
-            console.log('handeling..', selection)
+            // TODO:
+            console.log('handeling..', selection)   
+            break 
     }
 }
 
 const lendingPoolMenu = async () => {
     const result = await inquirer.prompt(prompts.menu.lendingPool)
-    return result.lendingPool
+    let selection
+    switch(result.lendingPool){
+        case 'activateReserve':
+            selection = await inquirer.prompt(prompts.lendingPool.activateReserve)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'batchInitReserve':
+            selection = await inquirer.prompt(prompts.lendingPool.batchInitReserve)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'configureReserveAsCollateral':
+            selection = await inquirer.prompt(prompts.lendingPool.configureReserveAsCollateral)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'deactivateReserve':
+            selection = await inquirer.prompt(prompts.lendingPool.deactivateReserve)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'disableBorrowingOnReserve':
+            selection = await inquirer.prompt(prompts.lendingPool.disableBorrowingOnReserve)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'disableReserveStableRate':
+            selection = await inquirer.prompt(prompts.lendingPool.disableReserveStableRate)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'enableBorrowingOnReserve':
+            selection = await inquirer.prompt(prompts.lendingPool.enableBorrowingOnReserve)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'enableReserveStableRate':
+            selection = await inquirer.prompt(prompts.lendingPool.enableReserveStableRate)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'freezeReserve':
+            selection = await inquirer.prompt(prompts.lendingPool.freezeReserve)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'setReserveFactor':
+            selection = await inquirer.prompt(prompts.lendingPool.setReserveFactor)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'setReserveInterestRateStrategyAddress':
+            selection = await inquirer.prompt(prompts.lendingPool.setReserveInterestRateStrategyAddress)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'unfreezeReserve':
+            selection = await inquirer.prompt(prompts.lendingPool.unfreezeReserve)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'updateAtoken':
+            selection = await inquirer.prompt(prompts.lendingPool.updateAtoken)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'updateVariableDebtToken':
+            selection = await inquirer.prompt(prompts.lendingPool.updateVariableDebtToken)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+        case 'updateStableDebtToken':
+            selection = await inquirer.prompt(prompts.lendingPool.updateStableDebtToken)
+            // TODO:
+            console.log('handeling..', selection)   
+            break
+    }
 }
 
 
