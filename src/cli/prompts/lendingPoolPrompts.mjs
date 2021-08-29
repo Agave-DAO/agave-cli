e632b0d061e225dc7d6cf34ccc287efa55c67cef
greenhornet <HoneyIsMoney@protonMail.com>
2021-08-29 11:33:43 +0400

    create lending pool prompts

---
+++ src/cli/prompts/lendingPoolPrompts.mjs
@@ -0,0 +1,145 @@
+// TODO: CUSTOM SOLIDITY DATA TYPES
+const batchInitReserve = []
+const updateAToken = []
+const updateStableDebtToken = []
+const updateVariableDebtToken = []
+
+const enableBorrowingOnReserve = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+    {
+        type: 'confirm',
+        name: 'stableBorrowRateEnabled',
+        message:
+            'stableBorrowRateEnabled: True if stable borrow rate needs to be enabled by default on this reserve',
+    },
+]
+
+const disableBorrowingOnReserve = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+]
+
+const configureReserveAsCollateral = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+    {
+        type: 'number',
+        name: 'ltv',
+        message: 'ltv: The loan to value of the asset when used as collateral',
+    },
+    {
+        type: 'number',
+        name: 'liquidationThreshold',
+        message:
+            'liquidationThreshold: The threshold at which loans using this asset as collateral will be considered undercollateralized',
+    },
+    {
+        type: 'number',
+        name: 'liquidationBonus',
+        message:
+            'liquidationThreshold: liquidationBonus The bonus liquidators receive to liquidate this asset. The values is always above 100%. A value of 105% means the liquidator will receive a 5% bonus',
+    },
+]
+
+const enableReserveStableRate = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+]
+
+const disableReserveStableRate = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+]
+
+const activateReserve = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+]
+
+const deactivateReserve = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+]
+
+const freezeReserve = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+]
+
+const unfreezeReserve = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+]
+
+const setReserveFactor = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+    {
+        type: 'number',
+        name: 'reserveFactor',
+        message: 'reserveFactor: The new reserve factor of the reserve',
+    },
+]
+
+const setReserveInterestRateStrategyAddress = [
+    {
+        type: 'input',
+        name: 'asset',
+        message: 'asset: The address of the underlying asset of the reserve',
+    },
+    {
+        type: 'input',
+        name: 'rateStrategyAddress',
+        message:
+            'rateStrategyAddress The new address of the interest strategy contract',
+    },
+]
+
+export default {
+    batchInitReserve,
+    updateAToken,
+    updateStableDebtToken,
+    updateVariableDebtToken,
+    enableBorrowingOnReserve,
+    disableBorrowingOnReserve,
+    configureReserveAsCollateral,
+    enableReserveStableRate,
+    disableReserveStableRate,
+    activateReserve,
+    deactivateReserve,
+    freezeReserve,
+    unfreezeReserve,
+    setReserveFactor,
+    setReserveInterestRateStrategyAddress,
+}
