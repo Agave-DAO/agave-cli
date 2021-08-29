bd22362ec76b57ebfc2ed99dbb294e951c047fdf
greenhornet <HoneyIsMoney@protonMail.com>
2021-08-29 11:56:39 +0400

    add lending pool prompts

--- src/cli/prompts/index.mjs
+++ src/cli/prompts/index.mjs
@@ -1,7 +1,8 @@
 import menu from "./menuPrompt.mjs";
 import garden from "./gardenPrompts.mjs";
 import emergencyAgent from "./emergencyAgentPrompts.mjs";
+import lendingPool from "./lendingPoolPrompts.mjs"
 
 export default {
-    menu, garden, emergencyAgent
+    menu, garden, emergencyAgent, lendingPool
 }
