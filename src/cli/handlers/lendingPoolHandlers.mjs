d359604a81946b888cd1f99ba2bc9f484cfcf9aa
honeyismoney <honeyismoney@protonmail.com>
2021-08-29 20:05:45 +0400

    created lending pool handlers

---
+++ src/cli/handlers/lendingPoolHandlers.mjs
@@ -0,0 +1,82 @@
+import Agave from '../../scripts/Agave.mjs'
+
+const lendingPoolHandlers = {
+    batchInitReserve: async () => {
+        // TODO:
+    },
+
+    updateAToken: async () => {
+        // TODO:
+    },
+
+    updateStableDebtToken: async () => {
+        // TODO:
+    },
+
+    updateVariableDebtToken: async () => {
+        // TODO:
+    },
+
+    enableBorrowingOnReserve: async (params) => {
+        const args = [params.asset, params.stableBorrowRateEnabled]
+        console.log(args)
+        await Agave.lendingPool.enableBorrowingOnReserve(args)
+    },
+
+    disableBorrowingOnReserve: async (params) => {
+        args = [params.asset]
+        await Agave.lendingPool.disableBorrowingOnReserve(args)
+    },
+
+    configureReserveAsCollateral: async (params) => {
+        args = [
+            params.asset,
+            params.ltv,
+            params.liquidationThreshold,
+            params.liquidationBonus,
+        ]
+        await Agave.lendingPool.configureReserveAsCollateral(args)
+    },
+
+    enableReserveStableRate: async (params) => {
+        args = [params.asset]
+        await Agave.lendingPool.enableReserveStableRate(args)
+    },
+
+    disableReserveStableRate: async (params) => {
+        args = [params.asset]
+        await Agave.lendingPool.disableReserveStableRate(args)
+    },
+
+    activateReserve: async (params) => {
+        args = [params.asset]
+        await Agave.lendingPool.activateReserve(args)
+    },
+
+    deactivateReserve: async (params) => {
+        args = [params.asset]
+        await Agave.lendingPool.deactivateReserve(args)
+    },
+
+    freezeReserve: async (params) => {
+        args = [params.asset]
+        await Agave.lendingPool.freezeReserve(args)
+    },
+
+    unfreezeReserve: async (params) => {
+        args = [params.asset]
+        await Agave.lendingPool.unfreezeReserve(args)
+    },
+
+    setReserveFactor: async (params) => {
+        args = [params.asset, params.reserveFactor]
+        await Agave.lendingPool.setReserveFactor(args)
+    },
+
+    setReserveInterestRateStrategyAddress: async (params) => {
+        args = [params.asset, params.rateStrategyAddress]
+        await Agave.lendingPool.setReserveInterestRateStrategyAddress(args)
+    },
+}
+
+export default lendingPoolHandlers
