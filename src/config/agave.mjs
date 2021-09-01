cca66b9f1f39a95268ba9491f8f8df25fe51fd6a
honeyismoney <honeyismoney@protonmail.com>
2021-09-01 21:05:11 +0400

    global agave object with all addresses & func sigs

---
+++ src/config/agave.mjs
@@ -0,0 +1,87 @@
+import {TaoVoting} from '../lib/daoApps.mjs'
+import { 
+    acl, 
+    cv_agent, 
+    emergency_admin_agent, 
+    aggregator, 
+    agreement, 
+    cv_voting, 
+    tao_voting,
+    tao_agent,
+    token_manager,
+    dao,
+    LendingPoolConfigurator,
+    LendingPoolAddressesProvider,
+    LendingPoolAddressesProviderRegistry
+} from './addresses.mjs'
+
+export default {
+    Kernel: {
+        address: dao
+    },
+    Acl: {
+        address: acl
+    },
+    TokenManager: {
+        address: token_manager
+    },
+    CVAgent: {
+        address: cv_agent
+    },
+    TAOAgent: {
+        address: tao_agent,
+    },
+    EmergencyAgent: {
+        address: emergency_admin_agent,
+    },
+    TAOVoting: {
+        address: tao_voting,
+        contract: TaoVoting
+    },
+    CVoting: {
+        address: cv_voting
+    },
+    Aggregator: {
+        address: aggregator
+    },
+    Agreement: {
+        address: agreement
+    },
+    LendingPoolConfigurator: {
+        address: LendingPoolConfigurator,
+        signatures: {
+            setFrozen: 'setFrozen(bool)',
+            activateReserve: 'activateReserve(address)',
+            configureReserveAsCollateral: 'configureReserveAsCollateral(address asset,uint256,uint256,uint256)',
+            deactivateReserve: 'deactivateReserve(address)',
+            disableBorrowingOnReserve: 'disableBorrowingOnReserve(address)',
+            enableBorrowingOnReserve: 'enableBorrowingOnReserve(address,bool)',
+            enableReserveStableRate: 'enableReserveStableRate(address)',
+            freezeReserve: 'freezeReserve(address)',
+            setReserveFactor: 'setReserveFactor(address,uint256)',
+            setReserveInterestRateStrategyAddress: 'setReserveInterestRateStrategyAddress(address,address)',
+            unfreezeReserve: 'unfreezeReserve(address)',
+        }
+    },
+    LendingPoolAddressesProvider: {
+        address: LendingPoolAddressesProvider,
+        signatures: {
+            setMarketId: 'setMarketId(string)',
+            setAddressAsProxy: 'setAddressAsProxy(bytes32,address)',
+            setLendingPoolImpl: 'setLendingPoolImpl(address)',
+            setLendingPoolConfiguratorImpl: 'setLendingPoolConfiguratorImpl(address)',
+            setLendingPoolCollateralManager: 'setLendingPoolCollateralManager(address)',
+            setPoolAdmin: 'setPoolAdmin(address)',
+            setEmergencyAdmin: 'setEmergencyAdmin(address)',
+            setPriceOracle: 'setPriceOracle(address)',
+            setLendingRateOracle: 'setLendingRateOracle(address)',
+        }
+    },
+    LendingPoolAddressesProviderRegistry: {
+        address: LendingPoolAddressesProviderRegistry,
+        signatures: {
+            registerAddressesProvider: 'registerAddressesProvider(address,uint256)',
+            unregisterAddressesProvider: 'unregisterAddressesProvider(address)'
+        }
+    }
+}
