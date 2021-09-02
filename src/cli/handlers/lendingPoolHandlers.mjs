542cc8d77177db091bbdf37a79fafea331f37222
honeyismoney <honeyismoney@protonmail.com>
2021-09-02 21:22:49 +0400

    update handlers

--- src/cli/handlers/lendingPoolHandlers.mjs
+++ src/cli/handlers/lendingPoolHandlers.mjs
@@ -77,6 +77,61 @@ const protocolHandlers = {
         const args = [params.asset, params.rateStrategyAddress]
         await scripts.protocol.setReserveInterestRateStrategyAddress(args)
     },
+
+    setMarketId: async (params) => {
+        const args = [params.id]
+        await scripts.protocol.setMarketId(args)
+    },
+
+    setAddressAsProxy: async (params) => {
+        const args = [params.id, params.implementationAddress]
+        await scripts.protocol.setAddressAsProxy(args)
+    },
+
+    setLendingPoolImpl: async (params) => {
+        const args = [params.asset]
+        await scripts.protocol.setLendingPoolImpl(args)
+    },
+
+    setLendingPoolConfiguratorImpl: async (params) => {
+        const args = [params.configurator]
+        await scripts.protocol.setLendingPoolConfiguratorImpl(args)
+    },
+
+    setLendingPoolCollateralManager: async (params) => {
+        const args = [params.manager]
+        await scripts.protocol.setLendingPoolCollateralManager(args)
+    },
+
+    setPoolAdmin: async (params) => {
+        const args = [params.admin]
+        await scripts.protocol.setPoolAdmin(args)
+    },
+
+    setEmergencyAdmin: async (params) => {
+        const args = [params.emergencyAdmin]
+        await scripts.protocol.setEmergencyAdmin(args)
+    },
+
+    setPriceOracle: async (params) => {
+        const args = [params.priceOracle]
+        await scripts.protocol.setPriceOracle(args)
+    },
+
+    setLendingRateOracle: async (params) => {
+        const args = [params.lendingRateOracle]
+        await scripts.protocol.setLendingRateOracle(args)
+    },
+
+    registerAddressesProvider: async (params) => {
+        const args = [params.addressProvider, params.id]
+        await scripts.protocol.registerAddressesProvider(args)
+    },
+
+    unregisterAddressesProvider: async (params) => {
+        const args = [params.addressProvider]
+        await scripts.protocol.unregisterAddressesProvider(args)
+    },
 }
 
 export default protocolHandlers
