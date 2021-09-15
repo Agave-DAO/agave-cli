b62845b25143bd7a0aa7f07f30802b3346dfcbe6
honeyismoney <honeyismoney@protonmail.com>
2021-09-15 15:25:22 +0400

    add emergency dao rinkeby

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -2,9 +2,10 @@ export default {
     rinkeby: {
         DAO: '0x1888d15ead3B33ed2d110f22d56f4fC338dEFE52',
         TaoAgent: '0xe34d132bde4724e8f142c209bc1268ed2370e845',
-        EmergencyDAO: '',
-        EmergencyAgent: '',
+        EmergencyDAO: '0xaE0043C667938f720235bF29C6a35C16507A5023',
+        EmergencyAgent: '0x934a0fdedea9d30a79f5fda90bdd1df7352e2a45',
         ///
         LendingPoolConfigurator: '0xaeB73F2ab2634C6759288d4d912227009E5bd55b',
+        LendingPoolAddressProvider: '',
     },
 }
