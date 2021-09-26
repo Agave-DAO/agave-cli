3632ef41a7ba062ffb279cb631032660b58237db
honeyismoney <honeyismoney@protonmail.com>
2021-09-26 18:35:33 +0400

    add permission table

--- src/commands/permissions.ts
+++ src/commands/permissions.ts
@@ -1,4 +1,25 @@
+import Table from "cli-table3"
+import { getDaoCache } from "../extensions/crispr-extentions"
+
 export default async () => {
-    console.log('inside command')
+    const cache = await getDaoCache(3)
+    const table = new Table({ head: ['App', 'Permission', 'Grantee', 'Manager'] })
+
+    for (let [app, context] of cache) {
+        const permissions = context.permissions
+        for (let [permission, settings] of permissions) {
+            const manager = settings['manager']
+            const grantees = settings['grantees']
+            for (let grantee of grantees.values()) {
+                table.push(
+                    [app, permission, grantee, manager]
+                )
+            }
+        }
+    }
+
+    console.log(table.toString())
+
+
     return 'main'
 }
